615fe7ae3b4a41b91a7c57a876b747f27dbe03c8
DanDean191 <github-new@inboxrefresh.com>
2024-02-16 16:48:02 +0000

    Moved the 'random' variable from the global scope to block scope within the getRandEvent function

--- trainingDays.js
+++ trainingDays.js
@@ -1,10 +1,9 @@
 const name = "Nala";
 
-// The scope of `random` is too loose
-const random = Math.floor(Math.random() * 3);
-
 const getRandEvent = () => {
-	if (random === 0) {
+    const random = Math.floor(Math.random() * 3);
+
+    if (random === 0) {
 		return "Marathon";
 	} else if (random === 1) {
 		return "Triathlon";
@@ -13,7 +12,6 @@ const getRandEvent = () => {
 	}
 };
 
-// The scope of `days` is too tight
 const getTrainingDays = (event) => {
 	let days;
 
@@ -28,14 +26,11 @@ const getTrainingDays = (event) => {
 	return days;
 };
 
-// The scope of `name` is too tight
 const logEvent = (name, event) => console.log(`${name}'s event is: ${event}`);
-
 const logTime = (name, days) => console.log(`${name}'s time to train is: ${days} days`);
 
 const event = getRandEvent();
 const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime
 
 logEvent(name, event);
 logTime(name, days);
