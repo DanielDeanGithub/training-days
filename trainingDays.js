d6860796849ec004d0eb0f23839193c523448c69
DanDean191 <github-new@inboxrefresh.com>
2024-02-16 16:44:28 +0000

    Passed name as the first argument to logEvent() and logTime()

--- trainingDays.js
+++ trainingDays.js
@@ -37,5 +37,5 @@ const event = getRandEvent();
 const days = getTrainingDays(event);
 // Define a `name` variable. Use it as an argument after updating logEvent and logTime
 
-logEvent(event);
-logTime(days);
+logEvent(name, event);
+logTime(name, days);
